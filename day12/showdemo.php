24b5ce303c2b174dd71c965ad716eb8098319150
jittapong020 <64309010020@chontech.co.th>
2021-12-02 16:30:46 +0700

    demo

--- day12/showdemo.php
+++ day12/showdemo.php
@@ -6,6 +6,8 @@ $dbname = "it64309010020";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+mysqli_set_charset($conn,"utf8");
+
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
