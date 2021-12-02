ccde533e329613a7164cac55f4bfb0d2e8210bb2
jittapong020 <64309010020@chontech.co.th>
2021-12-02 16:08:13 +0700

    demo

---
+++ day12/showdemo.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "it64309010020";
+$password = "12345678it";
+$dbname = "it64309010020";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM demo";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  echo "<table><tr><th>ID</th><th>Name</th></tr>";
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "<tr><td>"." id ".$row["id"]."</td><td>"." - name ".$row["name"]." - address".$row["address"]."</td></tr>";
+  }
+  echo "</table>";
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
